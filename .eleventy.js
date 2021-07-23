3f4a0d63d3d0abebdcac5796c31f06aabc1778db
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-07-23 10:45:18 -0400

    Finished day 5

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addWatchTarget('./src/sass/');
+  eleventyConfig.addPassthroughCopy('./src/img/');
 
   return {
     dir: {
-      input: "src",
-      output: "public",
-    },
+      input: 'src',
+      output: 'public'
+    }
   };
 };
